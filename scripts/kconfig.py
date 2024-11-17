c9eb74b3202fa1e226cb373b46ad0cca16f3011d
Jiamu Sun <barroit@linux.com>
2024-11-18 06:59:20 +0900

    kconfig: chdir to TREE before running genconfig
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/kconfig.py
+++ scripts/kconfig.py
@@ -5,15 +5,16 @@
 # You must provide TREE, CC, and LD in the process that starts this script.
 
 from sys import argv, exit
-from os import environ as env, getcwd, path, pathsep
+from os import environ as env, getcwd, path, pathsep, chdir
 from subprocess import run
 
 if len(argv) < 2:
 	exit(128)
 
 cmd = argv[1]
+ws  = env['TREE']
 cwd = getcwd()
-lib = path.join(cwd, 'lib')
+lib = path.join(ws, 'lib')
 pyp = env.get('PYTHONPATH')
 
 env['PYTHONPATH'] = (pyp + pathsep if pyp else '') + lib
@@ -28,6 +29,9 @@ elif cmd == 'genconfig':
 	pass
 else:
 	exit(128)
+
+if cwd != ws:
+	chdir(ws)
 
 res = run(argv[1:])
 exit(res.returncode)
