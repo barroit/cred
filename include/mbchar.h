07991539709833ce85bd3c50b646550796a8878d
Jiamu Sun <barroit@linux.com>
2024-11-20 22:43:28 +0900

    mbchar: add mbstatus()
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ include/mbchar.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later or MIT */
+/*
+ * Copyright 2024 Jiamu Sun <barroit@linux.com>
+ */
+
+#ifndef MBCHAR_H
+#define MBCHAR_H
+
+enum mbstatus {
+	MB_INVAL = -1,	/* unrecognized character */
+	MB_ASCII = 0,	/* character is ascii */
+	MB_DATA,	/* in the middle of a character */
+	MB_LBT2,	/* character is represented by 2 bytes */
+	MB_LBT3,
+	MB_LBT4,
+};
+
+/**
+ * mbstatus - get the mbstatus of a character
+ * @c: character to check
+ *
+ * Returns the status as an enum mbstatus value.
+ */
+enum mbstatus mbstatus(char c);
+
+#endif
