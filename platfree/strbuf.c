b9722bbb8471826e720b62dc8b77ea011b81cc87
Jiamu Sun <barroit@linux.com>
2024-12-01 23:05:38 +0900

    strbuf: sanitize path in sb_init_ws()
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- platfree/strbuf.c
+++ platfree/strbuf.c
@@ -205,6 +205,9 @@ void sb_init_ws(struct strbuf *sb, const xchar *name)
 {
 	memset(sb, 0, sizeof(*sb));
 	sb->off.ws = sb_puts(sb, name);
+
+	if (IS_ENABLED(CONFIG_STRBUF_SANITIZE_PATH))
+		sanitize_pth_sep(sb);
 }
 
 uint sb_pth_append(struct strbuf *sb, const xchar *name)
