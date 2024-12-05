2c205a8592560c77a3ef8f1da8e5acafad5daa44
Jiamu Sun <barroit@linux.com>
2024-12-05 14:40:53 +0900

    tests: init: remove floating point test
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- tests/unit/init.c
+++ tests/unit/init.c
@@ -9,14 +9,12 @@
 
 static int int39;
 static uint hex39;
-static float fp39;
 static xchar *str39;
 
 INIT_ROUTINE(init39)
 {
 	int39 = 3939;
 	hex39 = 0x3939;
-	fp39 = 39.39f;
 	str39 = XC("mikumiku");
 }
 
@@ -26,7 +24,6 @@ TESTDECL_ROUTINE(39check)
 {
 	USSERT_EQUAL(int39, 3939);
 	USSERT_EQUAL(hex39, 0x3939);
-	USSERT_EQUAL(fp39, 39.39f);
 	USSERT_STREQUAL(str39, XC("mikumiku"));
 }
 
