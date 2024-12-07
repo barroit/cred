cf9e7e4b5eee5b590909b52ef95e434f50e834e8
Jiamu Sun <barroit@linux.com>
2024-12-07 19:09:44 +0900

    mbctype: fix incorrect index mapping in _39mbctype
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- include/mbctype.h
+++ include/mbctype.h
@@ -9,19 +9,20 @@
 extern unsigned char _39mbctype[];
 
 enum mbctype {
+	_9CB,  /* continuation bytes */
 	_9A,   /* 1-byte */
 	_9B,   /* 2-byte */
 	_9C,   /* 3-byte */
 	_9D,   /* 4-byte */
-	_9CB,  /* continuation bytes */
 };
 
-#define __ismbctype(c, t) (_39mbctype[(int)(unsigned char)(c)] == t)
+#define __mbctype(c) _39mbctype[(int)(unsigned char)(c)]
+#define __ismbctype(c, t) (__mbctype(c) == t)
 
-#define ismb1(c) __ismbctype(c, _9A)
-#define ismb2(c) __ismbctype(c, _9B)
-#define ismb3(c) __ismbctype(c, _9C)
-#define ismb4(c) __ismbctype(c, _9D)
-#define ismbc(c) __ismbctype(c, _9CB)
+#define ismb1(c)  __ismbctype(c, _9A)
+#define ismb2(c)  __ismbctype(c, _9B)
+#define ismb3(c)  __ismbctype(c, _9C)
+#define ismb4(c)  __ismbctype(c, _9D)
+#define ismbcb(c) __ismbctype(c, _9CB)
 
 #endif /* NG39_MBTYPE_H */
