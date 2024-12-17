106b1137450556428df1dda5dfcec19d0b5f2f8c
Jiamu Sun <barroit@linux.com>
2024-12-18 02:39:06 +0900

    string: add __mbtowc()
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- include/string.h
+++ include/string.h
@@ -3,14 +3,23 @@
  * Copyright 2024 Jiamu Sun <barroit@linux.com>
  */
 
+/*
+ * For functions in this file, behavior is undefined if the character sequence
+ * is invalid.
+ */
+
 #ifndef NG39_STRING_H
 #define NG39_STRING_H
 
 #include_next <string.h>
 
+#include <stddef.h>
+
 #include "attr.h"
 #include "types.h"
 
 size_t __mbslen(const char *s) __leaf __pure;
+
+wchar_t __mbtowc(const char *seq) __leaf __pure;
 
 #endif /* NG39_STRING_H */
--- platfree/string.c
+++ platfree/string.c
@@ -18,3 +18,30 @@ size_t __mbslen(const char *s)
 
 	return len;
 }
+
+wchar_t __mbtowc(const char *seq)
+{
+	uint shift = 6;
+	uint mask = 0x1F;
+	wchar_t res = 0;
+	uint len = _39mbctype[(int)(unsigned char)*seq];
+
+	switch (len) {
+	case _9D:
+		res |= seq[3] & 0x3F;
+		shift += 6;
+		mask >>= 1;
+	case _9C:
+		res |= (seq[2] & 0x3F) << (shift - 6);
+		shift += 6;
+		mask >>= 1;
+	case _9B:
+		res |= ((seq[0] & mask) << shift) |
+		       ((seq[1] & 0x3F) << (shift - 6));
+		break;
+	default:
+		res = seq[0];
+	}
+
+	return res;
+}
