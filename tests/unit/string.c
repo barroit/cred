65f85a4b6ddeca40396335eaa36b7338b3a5e123
Jiamu Sun <barroit@linux.com>
2024-12-18 02:42:56 +0900

    tests: string: add __mbtowc test
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ tests/unit/string.c
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later or MIT
+/*
+ * Copyright 2024 Jiamu Sun <barroit@linux.com>
+ */
+
+#include "unitest.h"
+
+#include "string.h"
+
+#include <locale.h>
+
+TESTDECL_BEGIN();
+
+TESTDECL_ROUTINE(__mbtowc)
+{
+	wchar_t str[2] = { 0 };
+
+	str[0] = __mbtowc("M");
+	USSERT_STREQUAL_WC(str, L"M");
+
+	str[0] = __mbtowc("é");
+	USSERT_STREQUAL_WC(str, L"é");
+
+	str[0] = __mbtowc("ミ");
+	USSERT_STREQUAL_WC(str, L"ミ");
+
+	str[0] = __mbtowc("𐍈");
+	USSERT_STREQUAL_WC(str, L"𐍈");
+
+	str[0] = __mbtowc((char []){ 0xF8, 0x80, 0x80, 0x80, 0 });
+	USSERT_LT(str[0], 0xFF);
+}
+
+TESTDECL_END();
