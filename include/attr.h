ca87183004dbd1fc308369cf560b7c52c2398215
Jiamu Sun <barroit@linux.com>
2025-01-11 23:13:39 +0900

    attr: alias __attribute__ as __attr
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- include/attr.h
+++ include/attr.h
@@ -6,17 +6,19 @@
 #ifndef	NG39_ATTR_H
 #define NG39_ATTR_H
 
-#define __maybe_unused __attribute__((__unused__))
+#define __attr __attribute__
 
-#define __noreturn __attribute__((__noreturn__))
+#define __maybe_unused __attr((__unused__))
 
-#define __pure __attribute__((__pure__))
+#define __noreturn __attr((__noreturn__))
+
+#define __pure __attr((__pure__))
 
 #undef __nonnull
-#define __nonnull(...) __attribute__((__nonnull__(__VA_ARGS__)))
+#define __nonnull(...) __attr((__nonnull__(__VA_ARGS__)))
 
 #ifdef ANSI
-# define __printf(m, n) __attribute__((__format__(__printf__, m, n))) __nonnull(m)
+# define __printf(m, n) __attr((__format__(__printf__, m, n))) __nonnull(m)
 #else
 # define __printf(m, n)
 #endif
@@ -24,26 +26,26 @@
 #define __cold __attribute__((__cold__))
 
 #if __has_attribute(__access__)
-# define __read_only(...) __attribute__((__access__(__read_only__, __VA_ARGS__)))
+# define __read_only(...) __attr((__access__(__read_only__, __VA_ARGS__)))
 #else
 # define __read_only(...)
 #endif
 
-#define __warn_unused_result __attribute__((__warn_unused_result__))
+#define __warn_unused_result __attr((__warn_unused_result__))
 
-#define __leaf __attribute__((__leaf__))
+#define __leaf __attr((__leaf__))
 
 #undef __always_inline
-#define __always_inline inline __attribute__((__always_inline__))
+#define __always_inline inline __attr((__always_inline__))
 
-#define __used __attribute__((__used__))
+#define __used __attr((__used__))
 
-#define __constructor __attribute__((__constructor__))
+#define __constructor __attr((__constructor__))
 
-#define __section(x) __attribute__((__section__(x)))
+#define __section(x) __attr((__section__(x)))
 
-#define __cleanup(x) __attribute__((__cleanup__(x)))
+#define __cleanup(x) __attr((__cleanup__(x)))
 
-#define __malloc __attribute__((__malloc__))
+#define __malloc __attr((__malloc__))
 
 #endif /* NG39_ATTR_H */
