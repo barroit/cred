2d5a9f4427a23669ee5cd72f9c6414803c8938b6
Jiamu Sun <barroit@linux.com>
2025-01-13 15:42:21 +0900

    timestamp: fix inaccurate time on Windows
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- win32/timestamp.c
+++ win32/timestamp.c
@@ -7,29 +7,30 @@
 
 #include <windows.h>
 
+#include <stdio.h>
+
 #include "calc.h"
 #include "types.h"
 
 timestamp_t ts_now(void)
 {
 	int err;
-	static u64 freqh;
-	static u64 freql;
+	static u64 tps[2];
 	static u8 scale = maxof(scale);
 	LARGE_INTEGER tick;
 
 	if (scale == maxof(scale)) {
-		LARGE_INTEGER freq;
+		LARGE_INTEGER __tps;
 
-		err = !QueryPerformanceFrequency(&freq);
+		err = !QueryPerformanceFrequency(&__tps);
 		BUG_ON(err);
 
-		freqh = (1000000000ULL << 32) / (uint64_t)freq.QuadPart;
-		freql = freqh;
+		tps[0] = (1000000000ULL << 32) / (u64)__tps.QuadPart;
+		tps[1] = tps[0];
 		scale = 32;
 
-		while (freql >= 0x100000000ULL) {
-			freql >>= 1;
+		while (tps[1] >= 0x100000000ULL) {
+			tps[1] >>= 1;
 			scale--;
 		}
 	}
@@ -37,13 +38,16 @@ timestamp_t ts_now(void)
 	err = !QueryPerformanceCounter(&tick);
 	BUG_ON(err);
 
-	return (freqh * tick.HighPart) + ((freql * tick.LowPart) >> scale);
+	timestamp_t t1 = tps[0] * tick.HighPart;
+	timestamp_t t2 = (tps[1] * tick.LowPart) >> scale;
+
+	return t1 + t2;
 }
 
 void ts_mono(struct timespec *ts)
 {
 	timestamp_t t = ts_now();
 
-	ts->tv_sec = t / 10000000000;
-	ts->tv_nsec = t % 10000000000;
+	ts->tv_sec = t / 1000000000LL;
+	ts->tv_nsec = t % 1000000000LL;
 }
