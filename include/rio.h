2fda44bba13ddefca0fd2f735898455a6e562bf8
Jiamu Sun <barroit@linux.com>
2025-01-19 05:32:35 +0900

    rio: introduce rio (restartable io) API
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ include/rio.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later or MIT */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#ifndef NG39_RIO_H
+#define NG39_RIO_H
+
+#include "types.h"
+
+ssize_t rread(int fd, void *buf, size_t count);
+
+ssize_t rwrite(int fd, const void *buf, size_t count);
+
+#endif /* NG39_RIO_H */
---
+++ platfree/rio.c
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0-or-later or MIT
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#include "rio.h"
+
+#include <errno.h>
+#include <unistd.h>
+
+#ifdef _WIN32
+# include <io.h>
+# define read  _read
+# define write _write
+#endif
+
+#include "compiler.h"
+
+ssize_t rread(int fd, void *buf, size_t count)
+{
+	ssize_t nr;
+
+retry:
+	nr = read(fd, buf, count);
+
+	if (unlikely(nr < 0 && errno == EINTR))
+		goto retry;
+
+	return nr;
+}
+
+
+ssize_t rwrite(int fd, const void *buf, size_t count)
+{
+	ssize_t nr;
+
+retry:
+	nr = write(fd, buf, count);
+
+	if (unlikely(nr < 0 && errno == EINTR))
+		goto retry;
+
+	return nr;
+}
