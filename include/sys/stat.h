fb8099969700a8b95684ddc50cd8104e3084f92c
Jiamu Sun <barroit@linux.com>
2025-01-31 02:08:26 +0900

    sys/stat: support link type for win32
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ include/sys/stat.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later or MIT */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#ifndef NG39_SYS_STAT_H
+#define NG39_SYS_STAT_H
+
+#include_next <sys/stat.h>
+
+#ifdef _WIN32
+# define S_IFLNK 0xa000
+# if (S_IFLNK & S_IFMT) != S_IFLNK
+#  error (S_IFLNK & S_IFMT) != S_IFLNK
+# endif
+# define S_ISLNK(m) (((m) & S_IFMT) == S_IFREG)
+#endif
+
+#endif /* NG39_SYS_STAT_H */
