d1b7296290df2f7c76973c7614866e9a85ea5b8d
Jiamu Sun <barroit@linux.com>
2025-02-18 23:40:26 +0900

    noleak: introduce noleak()
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ platfree/noleak.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#include "noleak.h"
+
+#include "list.h"
+#include "xalloc.h"
+
+struct ref {
+	struct list_head list;
+	char carrier[];
+};
+
+static LIST_HEAD(refs);
+
+void __noleak(void *ptr, size_t size)
+{
+	size_t n = sizeof(struct ref) + size;
+	struct ref *ref = xmalloc(n);
+
+	list_add(&ref->list, &refs);
+	memcpy(ref->carrier, ptr, size);
+}
