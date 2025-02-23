ca0e0133f263641407b7b13b5fadd637128a790d
Jiamu Sun <barroit@linux.com>
2025-02-23 18:09:26 +0900

    win32: tests: param: replace printf with wprintf
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- tests/param/main.c
+++ tests/param/main.c
@@ -8,9 +8,15 @@
 #if defined(_WIN32) && defined(CONFIG_ENABLE_WCHAR)
 # pragma GCC diagnostic ignored "-Wmissing-prototypes"
 # define main wmain
+# include <unistd.h>
+# include <fcntl.h>
+# define fix_std_mode() _setmode(STDOUT_FILENO, _O_U8TEXT)
+#else
+# define fix_std_mode()
 #endif
 
 int main(int argc, const xchar **argv)
 {
+	fix_std_mode();
 	return __main(argc, argv);
 }
