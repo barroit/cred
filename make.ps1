d1ccc188e33be49a24df06d5a1543e95a54a72ab
Jiamu Sun <barroit@linux.com>
2025-02-24 23:00:14 +0900

    win32: build: use flags to match targets
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- make.ps1
+++ make.ps1
@@ -7,17 +7,21 @@
 
 param
 (
-	[ArgumentCompletions('configure', 'build', `
-			     'all', 'test', 'clean', 'distclean', `
-			     'menuconfig', 'install', 'uninstall')]
-	[string]$first
+	[ArgumentCompletions('menuconfig', 'configure', 'build', 'all',`
+			     'clean', 'distclean', 'install', 'uninstall')]
+	[string]$__target = 'build'
 )
 
+$ErrorActionPreference = 'Stop'
+
 Set-Alias error Write-Error
+Set-Alias rm_f  Remove-Item -Force -ErrorAction SilentlyContinue
+
+$BUILD_NAME = 'build.win32'
 
 $TOP   = $PSScriptRoot.Replace('\','/')
 $GEN   = "$TOP/include/generated"
-$BUILD = "$TOP/build.win32"
+$BUILD = "$TOP/$BUILD_NAME"
 
 $env:TOP   = $TOP
 $env:GEN   = $GEN
@@ -25,78 +29,101 @@ $env:BUILD = $BUILD
 
 $env:KCONFIG_CONFIG = '.config.win32'
 
-$targets = @('configure', 'build', 'all', 'test', 'clean', `
-	     'distclean', 'menuconfig', 'install', 'uninstall')
+$__menuconfig = 1 -shl 0
+$__configure  = 1 -shl 1
+$__build      = 1 -shl 2
+$__all	      = $__configure -bor $__build
+$__clean      = 1 -shl 3
+$__distclean  = 1 -shl 4
+$__install    = 1 -shl 5
+$__uninstall  = 1 -shl 6
+$__test       = 1 -shl 7
 
-if (! $first) {
-	# make
-	$target = 'build'
-} elseif ($first -in $targets) {
-	# make all
-	$target = $first
-} else {
-	# make LLVM=1
-	# or
-	# make LLVM=1 configure
-	# or
-	# make xxxx
-	$args = @($first) + $args
-	$target = 'build'
-	if ($args[-1] -in $targets) {
-		$target = $args[-1]
+switch -CaseSensitive ($__target) {
+'menuconfig' { $target = $__menuconfig; break }
+'configure'  { $target = $__configure;  break }
+'build'	     { $target = $__build;      break }
+'all'	     { $target = $__all;        break }
+'clean'	     { $target = $__clean;      break }
+'distclean'  { $target = $__distclean;  break }
+'install'    { $target = $__install;    break }
+'uninstall'  { $target = $__uninstall;  break }
+}
+
+if (!$target) {
+	switch -Wildcard ($__target) {
+	't/*'       { $target = $__test; break}
+	}
+
+	if (!$target) {
+		error "unknown target '$__target'"
 	}
 }
 
-if ($args | Where-Object { $_ -like 'GMAKE=*' }) {
-	$GMAKE = 1
-}
-
-$env:CC = 'clang'
-# Append an extension to make python correctly find that executable
+$env:CC = 'clang.exe'
 $env:LD = 'ld.lld.exe'
 
-if ($target -eq 'configure' -or $target -eq 'all') {
-	if ($GMAKE) {
-		$generator = 'Unix Makefiles'
-	} else {
-		$generator = 'Ninja'
-	}
-
-	cmake -G $generator -S . -B $BUILD
+if ($target -band $__menuconfig) {
+	python scripts/kconfig.py menuconfig
 }
 
-if ($target -eq 'build' -or $target -eq 'all') {
-	if (!(Test-Path .last_build)) {
-		echo win32 > .last_build
-	} elseif ((cat .last_build) -ne 'win32') {
+if ($target -band $__configure) {
+	if (!(Test-Path $BUILD/features.cmake)) {
+		if (!(Test-Path $GEN)) {
+			mkdir $GEN
+		}
+
+		scripts/cc-feature.py
+	}
+
+	cmake -G Ninja -S . -B $BUILD
+}
+
+if ($target -band $__build) {
+	if (!(Test-Path .last_build) -or (cat .last_build) -ne 'win32') {
 		echo win32 > .last_build
 	}
 
 	cmake --build $BUILD --parallel
 }
 
-if ($target -eq 'test') {
-	ctest --test-dir $BUILD/tests --parallel $env:NUMBER_OF_PROCESSORS
-} elseif ($target -eq 'clean') {
+if ($target -band $__clean) {
 	cmake --build $BUILD --target clean
-} elseif ($target -eq 'distclean') {
-	Remove-Item -Recurse -Force -ErrorAction SilentlyContinue`
-		    -Path include/generated
+}
 
-	Remove-Item -Force -ErrorAction SilentlyContinue `
-		    -Path $env:KCONFIG_CONFIG*
+if ($target -band $__distclean) {
+	$dotconfig = Get-ChildItem -Force $env:KCONFIG_CONFIG*
+	$buildgens = git ls-files --directory -o $BUILD_NAME
 
-	Remove-Item -Force -ErrorAction SilentlyContinue `
-		    -Path .last_build
-
-	Remove-Item -Force -ErrorAction SilentlyContinue `
-		    -Path *.manifest
-
-	$f = git ls-files --directory -o $BUILD
-	if ($f) {
-		Remove-Item -Recurse -Force -ErrorAction SilentlyContinue `
-			    -Path (git ls-files --directory -o $BUILD)
+	if (Test-Path include/generated) {
+		Remove-Item -Recurse include/generated
 	}
-} elseif ($target -eq 'menuconfig') {
-	python scripts/kconfig.py menuconfig
+	if ($dotconfig) {
+		Remove-Item $dotconfig
+	}
+	if (Test-Path .last_build) {
+		Remove-Item .last_build
+	}
+	if (Test-Path *.manifest) {
+		Remove-Item *.manifest
+	}
+	if ($buildgens) {
+		Remove-Item -Recurse $buildgens
+	}
+}
+
+$cpus = $env:NUMBER_OF_PROCESSORS
+
+if ($target -band $__test) {
+	if ($__target -eq 't/all') {
+		ctest --test-dir $BUILD/tests --parallel $cpus
+	} else {
+		$t = "$BUILD/$__target.exe"
+
+		if (!(Test-Path $t)) {
+			error "not a test '$t'"
+		}
+
+		& $t
+	}
 }
