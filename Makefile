barroit/cred

commit 5562a616183857bdc87cca9b410d574e563025c2

cmake: regenerate autoconf.h on first build per platform
Signed-off-by: Jiamu Sun <barroit@linux.com>

.gitignore
@@ -10,6 +10,7 @@ build.*/*
 !build.*/locale
 !build.*/scripts
 !build.*/t
+.last_build
 
 include/generated
 include/arch

CMakeLists.txt
@@ -17,21 +17,10 @@ set(SANFLAGS -fsanitize=address,undefined)
 file(MAKE_DIRECTORY ${GEN})
 
 include(lib/cmake.include)
-
-if(NOT EXISTS ${TREE}/$ENV{KCONFIG_CONFIG})
-  set(ENV{KCONFIG_CONFIG} ${TREE}/$ENV{KCONFIG_CONFIG}.def)
-  if(NOT EXISTS ${TREE}/$ENV{KCONFIG_CONFIG})
-    python3(scripts/kconfig.py alldefconfig)
-  endif()
-endif()
-
-python3(scripts/kconfig.py genconfig --header-path ${GEN}/autoconf.h)
 include_scp(lib/cmake.kconfig)
 
-if(EXISTS ${TREE}/.config)
-  set(ENV{CC} ${CONFIG_CC_COMMAND})
-  set(ENV{LD} ${CONFIG_LD_COMMAND})
-endif()
+set(ENV{CC} ${CONFIG_CC_COMMAND})
+set(ENV{LD} ${CONFIG_LD_COMMAND})
 
 set(prog_name ${CONFIG_PROG_NAME})
 if(CONFIG_SPEC_EXEC_NAME)
@@ -150,15 +139,18 @@ if(WIN32 AND NOT CONFIG_WIN32_GUI)
   list(FILTER pspec_src EXCLUDE REGEX \\.gui\\.c$)
 endif()
 add_library(platspec OBJECT ${pspec_src})
+add_dependencies(platspec genconfig)
 
 file(GLOB pfree_src LIST_DIRECTORIES false platfree/*.c)
 if(NOT HAVE_INTL)
   list(FILTER pfree_src EXCLUDE REGEX gettext\.c)
 endif()
 add_library(platfree OBJECT ${pfree_src})
+add_dependencies(platfree genconfig)
 
 file(GLOB cmd_src LIST_DIRECTORIES false commands/*.c)
 add_library(command OBJECT ${cmd_src})
+add_dependencies(command genconfig)
 
 if(CONFIG_PROG_WITH_SANITIZER)
   target_compile_options(platfree PRIVATE ${SANFLAGS})
@@ -192,6 +184,7 @@ if(WIN32)
 endif()
 
 add_executable(${exec_name} ${source})
+add_dependencies(${exec_name} genconfig)
 target_link_libraries(${exec_name} PRIVATE command)
 
 add_compile_definitions(_GNU_SOURCE)

Makefile
@@ -22,9 +22,18 @@ endif
 
 export CC LD
 
-.PHONY: configure build all
-
 build:
+
+.PHONY: configure build all last_build
+
+last_build:
+	@if [ ! -f .last_build ]; then			\
+		echo unix > .last_build;		\
+	elif [ "$$(cat .last_build)" != unix ]; then	\
+		echo unix > .last_build;		\
+	fi
+
+build: last_build
 	@cmake --build $(BUILD) --parallel
 
 configure:
@@ -41,6 +50,7 @@ distclean:
 	@rm -rf include/generated
 	@rm -f include/arch
 	@rm -f $(KCONFIG_CONFIG)*
+	@rm -f .last_build
 	@git ls-files --directory -o $(BUILD) | xargs rm -rf
 
 .PHONY: menuconfig

lib/cmake.kconfig
@@ -1,5 +1,17 @@
 # SPDX-License-Identifier: GPL-3.0-or-later or MIT
 
+if(NOT EXISTS ${TREE}/$ENV{KCONFIG_CONFIG})
+  set(ENV{KCONFIG_CONFIG} ${TREE}/$ENV{KCONFIG_CONFIG}.def)
+  if(NOT EXISTS ${TREE}/$ENV{KCONFIG_CONFIG})
+    python3(scripts/kconfig.py alldefconfig)
+  endif()
+endif()
+
+add_custom_target(genconfig ${Python3_EXECUTABLE} scripts/kconfig.py
+		  genconfig --header-path ${GEN}/autoconf.h
+		  DEPENDS ${TREE}/.last_build
+		  WORKING_DIRECTORY ${TREE})
+
 file(STRINGS $ENV{KCONFIG_CONFIG} lines ENCODING UTF-8)
 
 foreach(line ${lines})

make.ps1
@@ -61,10 +61,17 @@ if ($target -eq 'configure' -or $target -eq 'all') {
 	} else {
 		$generator = 'Ninja'
 	}
+
 	cmake -G $generator -S . -B $BUILD
 }
 
 if ($target -eq 'build' -or $target -eq 'all') {
+	if (!(Test-Path .last_build)) {
+		echo win32 > .last_build
+	} elseif ((cat .last_build) -ne 'win32') {
+		echo win32 > .last_build
+	}
+
 	cmake --build $BUILD --parallel
 }
 
@@ -78,6 +85,9 @@ if ($target -eq 'test') {
 
 	Remove-Item -Force -ErrorAction SilentlyContinue `
 		    -Path $env:KCONFIG_CONFIG*
+
+	Remove-Item -Force -ErrorAction SilentlyContinue `
+		    -Path .last_build
 
 	Remove-Item -Force -ErrorAction SilentlyContinue `
 		    -Path *.manifest

commit 288953cf21d04c334827b0ddf224d6ef7896a161

cmake: fix autoconf.h generation breaking cmake dependency tracking
Fixes: 5562a6161838 ("cmake: regenerate autoconf.h on first build per platform")
Signed-off-by: Jiamu Sun <barroit@linux.com>

.gitignore
@@ -10,7 +10,7 @@ build.*/*
 !build.*/locale
 !build.*/scripts
 !build.*/t
-.last_build
+.lastplat
 
 include/generated
 include/arch

CMakeLists.txt
@@ -17,7 +17,9 @@ set(SANFLAGS -fsanitize=address,undefined)
 file(MAKE_DIRECTORY ${GEN})
 
 include(lib/cmake.include)
+
 include_scp(lib/cmake.kconfig)
+
 include_scp(lib/cmake.features)
 include(${BUILD}/features.cmake)
 

Makefile
@@ -11,8 +11,6 @@ export TOP   := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 export GEN   := $(TOP)/include/generated
 export BUILD := $(TOP)/build.unix
 
-export KCONFIG_CONFIG := .config.unix
-
 ifneq ($(LLVM),)
 CC := clang
 LD := ld.lld
@@ -23,30 +21,58 @@ endif
 
 export CC LD
 
-FEATURES_CMAKE := $(BUILD)/features.cmake
+export LASTPLAT := $(TOP)/.lastplat
+
+export DOTCONFIG := $(TOP)/.config.unix
+export DEFCONFIG := $(DOTCONFIG).def
+
+ifneq ($(wildcard $(DOTCONFIG)),)
+RELCONFIG := $(DOTCONFIG)
+else
+RELCONFIG := $(DEFCONFIG)
+endif
+
+export RELCONFIG
+export KCONFIG_CONFIG := $(RELCONFIG)
+
+ifneq ($(wildcard $(DOTCONFIG)),)
+ifneq ($(wildcard $(DEFCONFIG)),)
+RECONFIGURE  := configure
+RM_DEFCONFIG := rm_defconifg
+endif
+endif
 
 build:
 
-.PHONY: configure build all last_build
+.PHONY: menuconfig rm_defconifg configure lastplat build all
+
+menuconfig:
+	@scripts/kconfig.py menuconfig
 
 $(GEN):
 	@mkdir $@
 
-$(FEATURES_CMAKE): $(GEN)
+$(BUILD)/features.cmake: $(GEN)
 	@scripts/cc-feature.py cmake
 
-last_build:
-	@if [ ! -f .last_build ]; then			\
-		echo unix > .last_build;		\
-	elif [ "$$(cat .last_build)" != unix ]; then	\
-		echo unix > .last_build;		\
+$(RELCONFIG):
+	@scripts/kconfig.py alldefconfig
+
+rm_defconifg:
+	@rm $(DEFCONFIG)
+
+configure: $(BUILD)/features.cmake $(RELCONFIG) $(RM_DEFCONFIG)
+	@$(REMCONFIG)
+	@cmake -S . -B $(BUILD) $(EXTOPT)
+
+lastplat:
+	@if [ -f $(BUILD)/CMakeCache.txt ] &&			\
+	    [ "$$(cat $(LASTPLAT) 2>&1 )" != unix ]; then	\
+		echo unix > $(LASTPLAT);			\
 	fi
 
-build: last_build
+build: lastplat $(RECONFIGURE)
 	@cmake --build $(BUILD) --parallel
-
-configure: $(FEATURES_CMAKE)
-	@cmake -S . -B $(BUILD) $(EXTOPT)
 
 all: configure build
 
@@ -58,14 +84,9 @@ clean:
 distclean:
 	@rm -rf include/generated
 	@rm -f include/arch
-	@rm -f $(KCONFIG_CONFIG)*
-	@rm -f .last_build
+	@rm -f $(DOTCONFIG)*
+	@rm -f $(LASTPLAT)
 	@git ls-files --directory -o $(BUILD) | xargs rm -rf
-
-.PHONY: menuconfig
-
-menuconfig:
-	@scripts/kconfig.py menuconfig
 
 __tests := $(wildcard $(BUILD)/t/*.t)
 tests   := $(patsubst $(BUILD)/%,%,$(__tests))

lib/cmake.features
@@ -2,6 +2,6 @@
 
 add_custom_command(OUTPUT ${GEN}/features.h
 		   COMMAND ${python3} scripts/cc-feature.py header
-		   DEPENDS ${TOP}/.last_build WORKING_DIRECTORY ${TOP})
+		   DEPENDS $ENV{LASTPLAT} WORKING_DIRECTORY ${TOP})
 
 add_custom_target(feature DEPENDS ${GEN}/features.h)

lib/cmake.kconfig
@@ -1,32 +1,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later or MIT
 
-set(KCONFIG_CONFIG $ENV{KCONFIG_CONFIG})
-
-if(NOT EXISTS ${TOP}/$ENV{KCONFIG_CONFIG})
-  set(ENV{KCONFIG_CONFIG} ${TOP}/$ENV{KCONFIG_CONFIG}.def)
-  if(NOT EXISTS ${TOP}/$ENV{KCONFIG_CONFIG})
-    python3(scripts/kconfig.py alldefconfig)
-  endif()
-endif()
-
-# It's okay to have only a .config.xxx.def on disk with $KCONFIG_CONFIG set to
-# .config.xxx, as genconfig will look up .config.xxx.def.
 add_custom_command(OUTPUT ${GEN}/autoconf.h
-		   COMMAND ${Python3_EXECUTABLE} scripts/kconfig.py
+		   COMMAND ${python3} scripts/kconfig.py
 		   genconfig --header-path ${GEN}/autoconf.h
-		   DEPENDS ${TOP}/.last_build
-			   ${KCONFIG_CONFIG}
-			   ${KCONFIG_CONFIG}.def
+		   COMMAND ${CMAKE_COMMAND} -E touch ${GEN}/autoconf.h
+		   DEPENDS $ENV{LASTPLAT} $ENV{RELCONFIG}
 		   WORKING_DIRECTORY ${TOP})
 
 add_custom_target(genconfig DEPENDS ${GEN}/autoconf.h)
 
-file(STRINGS $ENV{KCONFIG_CONFIG} lines ENCODING UTF-8)
+file(STRINGS $ENV{RELCONFIG} lines ENCODING UTF-8)
 
 foreach(line ${lines})
   if(line MATCHES "^(CONFIG_[^=]+)=(\"?)([^\"]*)(\"?)$")
     set(${CMAKE_MATCH_1} ${CMAKE_MATCH_3} PARENT_SCOPE)
   endif()
 endforeach()
-
-set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${TOP}/.config)

make.ps1
@@ -17,6 +17,8 @@ $ErrorActionPreference = 'Stop'
 Set-Alias error Write-Error
 Set-Alias rm_f  Remove-Item -Force -ErrorAction SilentlyContinue
 
+$0 = $MyInvocation.MyCommand.Path
+
 function export
 {
 	param([string]$str)
@@ -31,11 +33,33 @@ function export
 
 $BUILD_NAME = 'build.win32'
 
-export TOP="$($PSScriptRoot.Replace('\','/'))"
-export GEN="$TOP/include/generated"
-export BUILD="$TOP/$BUILD_NAME"
+export TOP=$($PSScriptRoot.Replace('\','/'))
+# $TOP = $TOP.TrimEnd('/')
 
-export KCONFIG_CONFIG=".config.win32"
+export GEN=$TOP/include/generated
+export BUILD=$TOP/$BUILD_NAME
+
+export CC=clang.exe
+export LD=ld.lld.exe
+
+export LASTPLAT=$TOP/.lastplat
+
+export DOTCONFIG=$TOP/.config.win32
+export DEFCONFIG=$DOTCONFIG.def
+
+if (Test-Path $DOTCONFIG) {
+	$RELCONFIG = $DOTCONFIG
+} else {
+	$RELCONFIG = $DEFCONFIG
+}
+
+export RELCONFIG=$RELCONFIG
+export KCONFIG_CONFIG=$RELCONFIG
+
+if ((Test-Path $DOTCONFIG) -and (Test-Path $DEFCONFIG)) {
+	$RECONFIGURE  = 1
+	$RM_DEFCONFIG = 1
+}
 
 $__menuconfig = 1 -shl 0
 $__configure  = 1 -shl 1
@@ -68,9 +92,6 @@ if (!$target) {
 	}
 }
 
-export CC='clang.exe'
-export LD='ld.lld.exe'
-
 if ($target -band $__menuconfig) {
 	python scripts/kconfig.py menuconfig
 }
@@ -84,12 +105,26 @@ if ($target -band $__configure) {
 		scripts/cc-feature.py cmake
 	}
 
+	if (!(Test-Path $RELCONFIG)) {
+		scripts/kconfig.py alldefconfig
+	}
+
+	if ($RM_DEFCONFIG) {
+		rm $DEFCONFIG
+		Remove-Variable RM_DEFCONFIG
+	}
+
 	cmake -G Ninja -S . -B $BUILD
 }
 
 if ($target -band $__build) {
-	if (!(Test-Path .last_build) -or (cat .last_build) -ne 'win32') {
-		echo win32 > .last_build
+	if (!(Test-Path $LASTPLAT) -or (cat $LASTPLAT) -ne 'win32') {
+		echo win32 > $LASTPLAT
+	}
+
+	if ($RECONFIGURE) {
+		& $0 configure
+		Remove-Variable RECONFIGURE
 	}
 
 	cmake --build $BUILD --parallel
@@ -109,8 +144,8 @@ if ($target -band $__distclean) {
 	if ($dotconfig) {
 		Remove-Item $dotconfig
 	}
-	if (Test-Path .last_build) {
-		Remove-Item .last_build
+	if (Test-Path $LASTPLAT) {
+		Remove-Item $LASTPLAT
 	}
 	if (Test-Path *.manifest) {
 		Remove-Item *.manifest

scripts/kconfig.py
@@ -23,6 +23,7 @@ env['KCONFIG_FUNCTIONS'] = 'Kinclude'
 
 if cmd == 'menuconfig':
 	env['MENUCONFIG_STYLE'] = 'aquatic'
+	env['KCONFIG_CONFIG'] = env['DOTCONFIG']
 elif cmd == 'alldefconfig':
 	pass
 elif cmd == 'genconfig':