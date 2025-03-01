barroit/cred

commit b6813bee5d8dddc944320be77aa26651961ed438

win32: sanitizer: add GUI-spec ASan death callback
Signed-off-by: Jiamu Sun <barroit@linux.com>

win32/sanitizer.gui.c
@@ -1,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later or MIT
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#include <sanitizer/asan_interface.h>
+
+#include <stdlib.h>
+
+#include "init.h"
+
+INIT_ROUTINE(death_cb)
+{
+	__sanitizer_set_death_callback(__exit_show_step);
+}

commit 001d0ca5b0ed82d85af1579baa402b17f152c9f4

win32: sanitizer: fix link error when !CONFIG_ENABLE_SANITIZER
Fixes: b6813bee5d8d ("win32: sanitizer: add GUI-spec ASan death callback")
Signed-off-by: Jiamu Sun <barroit@linux.com>

win32/sanitizer.gui.c
@@ -3,7 +3,12 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
-#include <sanitizer/asan_interface.h>
+#ifdef CONFIG_ENABLE_SANITIZER
+# include <sanitizer/asan_interface.h>
+#else
+# include "compiler.h"
+# define __sanitizer_set_death_callback NOOP
+#endif
 
 #include <stdlib.h>
 