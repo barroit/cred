barroit/cred

commit bbf89d92d947c72ba422b9d288641157b25ffcaf

build: introduce NG39_NO_STD_HDR_PATCH
Signed-off-by: Jiamu Sun <barroit@linux.com>

include/stdlib.h
@@ -5,8 +5,8 @@
 
 #ifndef NG39_STDLIB_H
 #define NG39_STDLIB_H
-
 #include_next <stdlib.h>
+#ifndef NG39_NO_STD_HDR_PATCH
 
 #include "attr.h"
 
@@ -25,4 +25,5 @@ void __noreturn __exit(int status);
 int setenv(const char *name, const char *value, int overwrite);
 #endif
 
+#endif /* NG39_NO_STD_HDR_PATCH */
 #endif /* NG39_STDLIB_H */

include/string.h
@@ -10,8 +10,8 @@
 
 #ifndef NG39_STRING_H
 #define NG39_STRING_H
-
 #include_next <string.h>
+#ifndef NG39_NO_STD_HDR_PATCH
 
 #include <stddef.h>
 
@@ -35,4 +35,5 @@ char *mbsws(const char *s);
 
 wchar_t *wcsws(const wchar_t *s);
 
+#endif /* NG39_NO_STD_HDR_PATCH */
 #endif /* NG39_STRING_H */

include/unistd.h
@@ -5,12 +5,9 @@
 
 #ifndef NG39_UNISTD_H
 #define NG39_UNISTD_H
-
-#ifdef __unix__
-
-#include_next <unistd.h>
-
-#else /* __unix__ */
+#if defined(__unix__)
+# include_next <unistd.h>
+#elif !defined(NG39_NO_STD_HDR_PATCH)
 
 #include <direct.h>
 #include <io.h>
@@ -27,5 +24,4 @@
 #define F_OK 00
 
 #endif /* __unix__ */
-
 #endif /* NG39_UNISTD_H */

commit ecd1e5ba8620f42670dd52a412897b1d1e3f2e4a

build: fix incorrect macro guard for patch blocks
Fixes: bbf89d92d947 ("build: introduce NG39_NO_STD_HDR_PATCH")
Signed-off-by: Jiamu Sun <barroit@linux.com>

include/stdlib.h
@@ -3,10 +3,13 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
-#ifndef NG39_STDLIB_H
-#define NG39_STDLIB_H
 #include_next <stdlib.h>
+
 #ifndef NG39_NO_STD_HDR_PATCH
+#ifndef NG39_STDLIB_PATCH_H
+#define NG39_STDLIB_PATCH_H
+
+#ifdef _WIN32
 
 #include "attr.h"
 
@@ -21,9 +24,9 @@ void __noreturn __exit(int status);
 # define EXIT_CONOUT 0
 #endif
 
-#ifdef _WIN32
 int setenv(const char *name, const char *value, int overwrite);
-#endif
 
+#endif /* _WIN32 */
+
+#endif /* NG39_STDLIB_PATCH_H */
 #endif /* NG39_NO_STD_HDR_PATCH */
-#endif /* NG39_STDLIB_H */

include/string.h
@@ -3,15 +3,16 @@
  * Copyright 2024, 2025 Jiamu Sun <barroit@linux.com>
  */
 
+#include_next <string.h>
+
 /*
  * For functions in this file, behavior is undefined if the character sequence
  * is invalid.
  */
 
-#ifndef NG39_STRING_H
-#define NG39_STRING_H
-#include_next <string.h>
 #ifndef NG39_NO_STD_HDR_PATCH
+#ifndef NG39_STRING_PATCH_H
+#define NG39_STRING_PATCH_H
 
 #include <stddef.h>
 
@@ -35,5 +36,5 @@ char *mbsws(const char *s);
 
 wchar_t *wcsws(const wchar_t *s);
 
+#endif /* NG39_STRING_PATCH_H */
 #endif /* NG39_NO_STD_HDR_PATCH */
-#endif /* NG39_STRING_H */

include/unistd.h
@@ -3,11 +3,15 @@
  * Copyright 2024 Jiamu Sun <barroit@linux.com>
  */
 
-#ifndef NG39_UNISTD_H
-#define NG39_UNISTD_H
-#if defined(__unix__)
+#ifdef __unix__
 # include_next <unistd.h>
-#elif !defined(NG39_NO_STD_HDR_PATCH)
+#endif
+
+#ifndef NG39_NO_STD_HDR_PATCH
+#ifndef NG39_UNISTD_PATCH_H
+#define NG39_UNISTD_PATCH_H
+
+#ifdef _WIN32
 
 #include <direct.h>
 #include <io.h>
@@ -23,5 +27,7 @@
 #define X_OK 01
 #define F_OK 00
 
-#endif /* __unix__ */
-#endif /* NG39_UNISTD_H */
+#endif /* _WIN32 */
+
+#endif /* NG39_UNISTD_PATCH_H */
+#endif /* NG39_NO_STD_HDR_PATCH */