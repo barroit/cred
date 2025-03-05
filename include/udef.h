a9c82e50fc72f3c61de5f560c133c80d80af1bd8
Jiamu Sun <barroit@linux.com>
2025-03-05 19:21:08 +0900

    win32: udef: make declaration of udef_no_console persistent
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- include/udef.h
+++ include/udef.h
@@ -20,8 +20,6 @@ extern int udef_termas_ts;	/* prefix message with timestamp */
 
 extern int udef_termas_pid;	/* prefix message with process id */
 
-#ifdef CONFIG_ENABLE_GUI
 extern int udef_no_console;	/* do not allocate console */
-#endif
 
 #endif /* NG39_UDEF_H */
