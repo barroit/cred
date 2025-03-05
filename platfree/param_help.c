barroit/cred

commit b80cb628cc72b3b2000486b9f4a922fbd7f7b8ca

param: move show_help() to param_help.c
Signed-off-by: Jiamu Sun <barroit@linux.com>

platfree/param_help.c
@@ -1,0 +1,162 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+#include "param.h"
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "strlist.h"
+
+static void show_cmd_usage(FILE *stream, const char **usage)
+{
+	const char *pref = "usage: ";
+	size_t __len = strlen("usage: ");
+
+	STRLIST(sl, SL__STORE_CHR);
+
+	while (*usage) {
+		const char *line = *usage;
+		const char *rest = line;
+
+		while (isalpha(*rest) || isspace(*rest))
+			rest++;
+
+		size_t len = rest - line;
+		size_t pad = __len + len;
+		size_t wrap = CONFIG_LINE_WRAP - __len;
+		char *str;
+
+		if ((CONFIG_LINE_WRAP >> 1) > pad)
+			wrap -= len;
+
+		fprintf(stream, "%*s%.*s", (int)__len, pref, (int)len, line);
+
+		if (!*rest) {
+			putc('\n', stream);
+			goto next;
+		}
+		sl_read_line_chr(&sl, rest, wrap);
+
+		str = sl_pop_chr(&sl);
+		fputs(str, stream);
+		putc('\n', stream);
+		free(str);
+
+		while ((str = sl_pop_chr(&sl))) {
+			fprintf(stream, "%*s%s\n", (int)pad, "", str);
+			free(str);
+		}
+
+next:
+		usage++;
+		pref = "or: ";
+	}
+
+	sl_destroy(&sl);
+	putc('\n', stream);
+}
+
+static void show_opt_usage(FILE *stream, struct opt *opts)
+{
+	int cnt = 0;
+	struct opt *opt;
+
+	STRLIST(sl, SL__STORE_CHR);
+
+	opt_for_each(opt, opts) {
+		switch (opt->mode) {
+		case OPTION__GROUP:
+			putc('\n', stream);
+			fputs(opt->__lnam, stream);
+			putc('\n', stream);
+		case OPTION__COMMAND:
+			continue;
+		}
+
+		const char *fmt;
+		size_t len = CONFIG_HELP_OPT_INDENT;
+
+		fprintf(stream, "%*s", (int)len, "");
+
+		if (opt->snam)
+			len += fprintf(stream, "-%c, ", opt->snam);
+
+		int no_neg = opt->flags & OPT_NO_NEG;
+
+		fmt = no_neg ? "--%s" : "--[no-]%s";
+		len += fprintf(stream, fmt, opt->__lnam);
+
+		if (opt->argh) {
+			char *occur = strpbrk(opt->argh, "()<>[]|");
+			int no_brac = occur != NULL;
+
+			if (opt->flags & OPT_OPT_ARG)
+				fmt = no_brac ? "[=%s]" : "[=<%s>]";
+			else
+				fmt = no_brac ? " %s" : " <%s>";
+			len += fprintf(stream, fmt, opt->argh);
+		}
+
+		if (len >= CONFIG_HELP_OPT_WRAP) {
+			putc('\n', stream);
+			len = 0;
+		}
+
+		size_t __pad = CONFIG_HELP_OPT_WRAP + 2;
+		size_t pad = __pad - len;
+		size_t wrap = CONFIG_LINE_WRAP - __pad;
+		char *str;
+
+		sl_read_line_chr(&sl, _(opt->usage), wrap);
+
+		str = sl_pop_chr(&sl);
+		fprintf(stream, "%*s%s\n", (int)pad, "", str);
+		free(str);
+
+		while ((str = sl_pop_chr(&sl))) {
+			fprintf(stream, "%*s%s\n", (int)__pad, "", str);
+			free(str);
+		}
+
+		cnt++;
+	}
+
+	sl_destroy(&sl);
+	if (cnt)
+		putc('\n', stream);
+}
+
+static void show_cmd_ext_usage(FILE *stream, const char **usage)
+{
+	while (*usage++);
+
+	uint lines = 0;
+
+	while (*usage) {
+		if (lines++)
+			putc('\n', stream);
+
+		fputs(_(*usage), stream);
+		putc('\n', stream);
+
+		usage++;
+	}
+}
+
+void param_show_help(const char **usage, struct opt *opts, int is_err)
+{
+	FILE *stream = is_err ? stderr : stdout;
+
+	show_cmd_usage(stream, usage);
+
+	show_opt_usage(stream, opts);
+
+	show_cmd_ext_usage(stream, usage);
+
+	exit(EXIT_CONOUT);
+}

include/param.h
@@ -6,6 +6,7 @@
 #ifndef NG39_PARAM_H
 #define NG39_PARAM_H
 
+#include "attr.h"
 #include "compiler.h"
 #include "gettext.h"
 #include "types.h"
@@ -73,8 +74,14 @@ struct opt {
 #define PRM_OPT_CMD  (1 << 3)
 #define PRM_NO_ARG   (1 << 4)
 
+#define opt_for_each(pos, opts) \
+	for (pos = opts; (pos)->mode != OPTION__END; (pos)++)
+
 int param_parse(int argc, const xchar **argv,
 		const char **usage, struct opt *opts, u32 flags);
+
+void __noreturn param_show_help(const char **usage,
+				struct opt *opts, int is_err);
 
 #define __opt_snam(s) __st_isascii(s) + s
 #define __opt_lnam(l) XC(l)

platfree/param.c
@@ -5,13 +5,12 @@
 
 #include "param.h"
 
-#include "bitopt.h"
-#include "iter.h"
+#include <stdlib.h>
+#include <string.h>
+
 #include "list.h"
 #include "strbuf.h"
 #include "strconv.h"
-#include "string.h"
-#include "strlist.h"
 #include "strtox.h"
 #include "termas.h"
 #include "xalloc.h"
@@ -23,9 +22,6 @@
 #ifdef __argc
 # undef __argc
 #endif
-
-#define opt_for_each(pos, opts) \
-	for (pos = opts; (pos)->mode != OPTION__END; (pos)++)
 
 #define OPT_SHORT_OPT (1 << 0)
 #define OPT_LONG_OPT  (1 << 1)
@@ -416,7 +412,7 @@ static int parse_cmd_arg(struct param *ctx)
 
 		if (!(ctx->flags & PRM_NO_HELP) &&
 		    str[0] == 'h' && str[1] == 0)
-			show_help(ctx->usage, ctx->opts, 0);
+			param_show_help(ctx->usage, ctx->opts, 0);
 		else if (str[0] == 0)
 			die(_("unknown option -"));
 
@@ -434,7 +430,7 @@ static int parse_cmd_arg(struct param *ctx)
 
 		if (!(ctx->flags & PRM_NO_HELP) &&
 		    xc_strcmp(str, XC("help")) == 0)
-			show_help(ctx->usage, ctx->opts, 0);
+			param_show_help(ctx->usage, ctx->opts, 0);
 
 		parse_long_opt(ctx, str);
 	}
@@ -496,159 +492,10 @@ int param_parse(int argc, const xchar **argv,
 		else
 			err = 0;
 
-		show_help(ctx.usage, ctx.opts, err);
+		param_show_help(ctx.usage, ctx.opts, err);
 	}
 
 	argc = ctx.outc + ctx.argc;
 	ctx.outv[argc] = 0;
 	return argc;
-}
-
-static void show_cmd_usage(FILE *stream, const char **usage)
-{
-	const char *pref = "usage: ";
-	size_t __len = strlen("usage: ");
-
-	STRLIST(sl, SL__STORE_CHR);
-
-	while (*usage) {
-		const char *line = *usage;
-		const char *rest = line;
-
-		while (isalpha(*rest) || isspace(*rest))
-			rest++;
-
-		size_t len = rest - line;
-		size_t pad = __len + len;
-		size_t wrap = CONFIG_LINE_WRAP - __len;
-		char *str;
-
-		if ((CONFIG_LINE_WRAP >> 1) > pad)
-			wrap -= len;
-
-		fprintf(stream, "%*s%.*s", (int)__len, pref, (int)len, line);
-
-		if (!*rest) {
-			putc('\n', stream);
-			goto next;
-		}
-		sl_read_line_chr(&sl, rest, wrap);
-
-		str = sl_pop_chr(&sl);
-		fputs(str, stream);
-		putc('\n', stream);
-		free(str);
-
-		while ((str = sl_pop_chr(&sl))) {
-			fprintf(stream, "%*s%s\n", (int)pad, "", str);
-			free(str);
-		}
-
-next:
-		usage++;
-		pref = "or: ";
-	}
-
-	sl_destroy(&sl);
-	putc('\n', stream);
-}
-
-static void show_opt_usage(FILE *stream, struct opt *opts)
-{
-	int cnt = 0;
-	struct opt *opt;
-
-	STRLIST(sl, SL__STORE_CHR);
-
-	opt_for_each(opt, opts) {
-		switch (opt->mode) {
-		case OPTION__GROUP:
-			putc('\n', stream);
-			fputs(opt->__lnam, stream);
-			putc('\n', stream);
-		case OPTION__COMMAND:
-			continue;
-		}
-
-		const char *fmt;
-		size_t len = CONFIG_HELP_OPT_INDENT;
-
-		fprintf(stream, "%*s", (int)len, "");
-
-		if (opt->snam)
-			len += fprintf(stream, "-%c, ", opt->snam);
-
-		int no_neg = opt->flags & OPT_NO_NEG;
-
-		fmt = no_neg ? "--%s" : "--[no-]%s";
-		len += fprintf(stream, fmt, opt->__lnam);
-
-		if (opt->argh) {
-			char *occur = strpbrk(opt->argh, "()<>[]|");
-			int no_brac = occur != NULL;
-
-			if (opt->flags & OPT_OPT_ARG)
-				fmt = no_brac ? "[=%s]" : "[=<%s>]";
-			else
-				fmt = no_brac ? " %s" : " <%s>";
-			len += fprintf(stream, fmt, opt->argh);
-		}
-
-		if (len >= CONFIG_HELP_OPT_WRAP) {
-			putc('\n', stream);
-			len = 0;
-		}
-
-		size_t __pad = CONFIG_HELP_OPT_WRAP + 2;
-		size_t pad = __pad - len;
-		size_t wrap = CONFIG_LINE_WRAP - __pad;
-		char *str;
-
-		sl_read_line_chr(&sl, _(opt->usage), wrap);
-
-		str = sl_pop_chr(&sl);
-		fprintf(stream, "%*s%s\n", (int)pad, "", str);
-		free(str);
-
-		while ((str = sl_pop_chr(&sl))) {
-			fprintf(stream, "%*s%s\n", (int)__pad, "", str);
-			free(str);
-		}
-
-		cnt++;
-	}
-
-	sl_destroy(&sl);
-	if (cnt)
-		putc('\n', stream);
-}
-
-static void show_cmd_ext_usage(FILE *stream, const char **usage)
-{
-	while (*usage++);
-
-	uint lines = 0;
-
-	while (*usage) {
-		if (lines++)
-			putc('\n', stream);
-
-		fputs(_(*usage), stream);
-		putc('\n', stream);
-
-		usage++;
-	}
-}
-
-static void show_help(const char **usage, struct opt *opts, int is_err)
-{
-	FILE *stream = is_err ? stderr : stdout;
-
-	show_cmd_usage(stream, usage);
-
-	show_opt_usage(stream, opts);
-
-	show_cmd_ext_usage(stream, usage);
-
-	exit(EXIT_CONOUT);
 }

commit 59aa4750a04311911ce7fea5a2a3c80c84593422

param_help: fix duplicate newline above OPT_GROUP output
Fixes: b80cb628cc72 ("param: move show_help() to param_help.c")
Signed-off-by: Jiamu Sun <barroit@linux.com>

platfree/param_help.c
@@ -72,7 +72,8 @@ static void show_opt_usage(FILE *stream, struct opt *opts)
 	opt_for_each(opt, opts) {
 		switch (opt->mode) {
 		case OPTION__GROUP:
-			putc('\n', stream);
+			if (opt != opts)
+				putc('\n', stream);
 			fputs(opt->__lnam, stream);
 			putc('\n', stream);
 		case OPTION__COMMAND: